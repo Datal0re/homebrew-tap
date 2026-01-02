522ffb9ca6497422dae61af7432ad4aa4a462368
Datal0re <42654566+Datal0re@users.noreply.github.com>
2026-01-02 04:30:58 -0600

    fix: ddd.rb URL pattern

--- Formula/ddd.rb
+++ Formula/ddd.rb
@@ -1,7 +1,7 @@
 class Ddd < Formula
   desc "CLI tool to process and explore exported ChatGPT conversation data"
   homepage "https://github.com/Datal0re/ddd"
-  url "https://registry.npmjs.org/@datal0re/ddd/-/@datal0re/ddd-0.1.0.tgz"
+  url "https://registry.npmjs.org/@datal0re/ddd/-/ddd-0.1.0.tgz"
   sha256 "1210af95a5ea6b84733afccb57d39a953551636b99bc5ff53bb1b744c2a16e61"
   license "MIT"
   head "https://github.com/Datal0re/ddd.git", branch: "main"
