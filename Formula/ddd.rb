1a3e5d7420277216324566e296dc2ccd9405923f
Datal0re <42654566+Datal0re@users.noreply.github.com>
2026-01-01 18:53:56 -0600

    Create ddd.rb

---
+++ Formula/ddd.rb
@@ -0,0 +1,16 @@
+class Ddd < Formula
+  desc "CLI tool to process and explore exported ChatGPT conversation data"
+  homepage "https://github.com/Datal0re/ddd"
+  url "https://registry.npmjs.org/@datal0re/ddd/-/@datal0re/ddd-0.1.0.tgz"
+  sha256 "1210af95a5ea6b84733afccb57d39a953551636b99bc5ff53bb1b744c2a16e61"
+  license "MIT"
+  head "https://github.com/Datal0re/ddd.git", branch: "main"
+  depends_on "node"
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*") => "ddd"
+  end
+  test do
+    system "#{bin}/ddd", "--version"
+  end
+end
