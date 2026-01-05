df174391e5b7c64f6b3123d572ae1804cc83734c
Datal0re <42654566+Datal0re@users.noreply.github.com>
2026-01-05 10:11:03 -0600

    ddd v0.1.7

--- Formula/ddd.rb
+++ Formula/ddd.rb
@@ -1,8 +1,8 @@
 class Ddd < Formula
   desc "CLI tool to process and explore exported ChatGPT conversation data"
   homepage "https://github.com/Datal0re/ddd"
-  url "https://registry.npmjs.org/@datal0re/ddd/-/ddd-0.1.6.tgz"
-  sha256 "978a730a72cf188f15794a7bd4158013f0d38ada608d928bce067dee8d8a43b2"
+  url "https://registry.npmjs.org/@datal0re/ddd/-/ddd-0.1.7.tgz"
+  sha256 "cac78d7f41e3d4f349d85cae016ac7264243afe656d7454fc37be2024b4bcba3"
   license "MIT"
   head "https://github.com/Datal0re/ddd.git", branch: "main"
   depends_on "node"
