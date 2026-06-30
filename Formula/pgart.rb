2b43209d1ea3291acf2a323e54e4631bee4c36fd
Datal0re <42654566+Datal0re@users.noreply.github.com>
2026-06-30 15:30:28 -0500

    Update pgart.rb URLs to version 1.1.0

--- Formula/pgart.rb
+++ Formula/pgart.rb
@@ -4,7 +4,7 @@ class Pgart < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://codeberg.org/Datal0re/pgart/releases/download/v1.0.0/pgart-darwin-all-v1.1.0.tar.gz"
+    url "https://codeberg.org/Datal0re/pgart/releases/download/v1.1.0/pgart-darwin-all-v1.1.0.tar.gz"
     sha256 "5874050a1b8b7a3920f88353c5eb5b6b39f70f56e063b94195ac3e9c5273350d"
     def install
       bin.install "pgart"
@@ -13,7 +13,7 @@ class Pgart < Formula
 
   # --- Linux x86_64 (AMD64) ---
   if OS.linux? && Hardware::CPU.intel?
-    url "https://codeberg.org/Datal0re/pgart/releases/download/v1.0.0/pgart-linux-amd64-v1.1.0.tar.gz"
+    url "https://codeberg.org/Datal0re/pgart/releases/download/v1.1.0/pgart-linux-amd64-v1.1.0.tar.gz"
     sha256 "f0cf125022aea9bf4b2dfa6c3773c49a57743776e3c8f8b5e85eb83e5ce41529"
 
     def install
@@ -23,7 +23,7 @@ class Pgart < Formula
 
   # --- Linux arm64 ---
   if OS.linux? && Hardware::CPU.arm?
-    url "https://codeberg.org/Datal0re/pgart/releases/download/v1.0.0/pgart-linux-arm64-v1.1.0.tar.gz"
+    url "https://codeberg.org/Datal0re/pgart/releases/download/v1.1.0/pgart-linux-arm64-v1.1.0.tar.gz"
     sha256 "b7c6a90095e4850953077044b1bc559aaf97d85a9ed4d2de64b3054352642f8d"
 
     def install
